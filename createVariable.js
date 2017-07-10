9b6567c12d6b13387d0fb8cfce04f37df1a451db
MattClarke97 <matthewclarke697@gmail.com>
2017-07-10 17:10:17 +0100

    Create createVariable.js

---
+++ createVariable.js
@@ -0,0 +1,13 @@
+//Variable to hold strings
+var strength = '50,000 pounds';
+var age = 20;
+var hasPet = false;
+console.log("How much stuff can a variable hold? ", strength);
+
+//Variable to hold other data types
+
+console.log(age);
+// Output: 15 (string)
+
+console.log(hasPet);
+// Output: False (boolean)
