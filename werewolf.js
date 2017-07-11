ddfb05c4ab69481df421c625f23900ee18029165
MattClarke97 <matthewclarke697@gmail.com>
2017-07-11 11:48:20 +0100

    Create werewolf.js

---
+++ werewolf.js
@@ -0,0 +1,6 @@
+var moonPhase = "full";
+if (moonPhase === "full") { //checks if the var is equal to the string.
+  	console.log("Howwwwlll!!");
+} else {
+  	console.log("I swear I am not a werewolf...");
+}
