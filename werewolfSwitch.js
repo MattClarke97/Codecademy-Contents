ff66dfb46d074ee116a61ae9c82acd5b922c44bf
MattClarke97 <matthewclarke697@gmail.com>
2017-07-11 12:10:48 +0100

    Create werewolfSwitch.js

---
+++ werewolfSwitch.js
@@ -0,0 +1,15 @@
+var moonPhase = "full";
+
+switch (moonPhase) {
+  case "full":
+    console.log("Howwwwlll!!");
+    break;
+  case "mostly full":
+    console.log("Arms and legs are getting hairier");
+    break;
+  case "mostly new":
+    console.log("Back on two feet");
+  default:
+    console.log("invalid moon phase");
+    break;
+}
