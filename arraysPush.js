2362eda44787ab2213ed46b314728d33eccec32f
MattClarke97 <matthewclarke697@gmail.com>
2017-07-11 13:43:17 +0100

    Create arraysPush.js

---
+++ arraysPush.js
@@ -0,0 +1,7 @@
+var bucketList = ["Paraglide","Fly","Snowboard"];
+var listItem = bucketList[2];
+
+bucketList.push("item 3", "item 4");
+console.log(listItem);
+console.log(bucketList.length); //.length returns the number of items in the array
+console.log(bucketList);
