917b4e66764d530026aa26d335ad0e5a785fd6f7
MattClarke97 <matthewclarke697@gmail.com>
2017-07-11 13:47:37 +0100

    Update arraysPush.js

--- arraysPush.js
+++ arraysPush.js
@@ -2,6 +2,7 @@ var bucketList = ["Paraglide","Fly","Snowboard"];
 var listItem = bucketList[2];
 
 bucketList.push("item 3", "item 4");
+//bucketList.pop()    This will delete the last item in an array.
 console.log(listItem);
 console.log(bucketList.length); //.length returns the number of items in the array
 console.log(bucketList);
